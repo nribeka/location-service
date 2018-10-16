aa9e719f35011cba2d96f75caa76d8cffbc1a1b2
Win Ribeka <win.ribeka@coxautoinc.com>
2018-10-15 23:30:01 -0400

    Adding basic spring boot application.

---
+++ src/main/java/com/meh/stuff/location/LocationDataApplication.java
@@ -0,0 +1,12 @@
+package com.meh.stuff.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationDataApplication.class, args);
+	}
+}
